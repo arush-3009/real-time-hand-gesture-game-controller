43f623367b65fc3b16cc4a4e7a56f4f18f84bd47
Arush Handa <handaarush1237@gmail.com>
2026-01-11 13:44:27 -0500

    Create folders for each gesture using os

--- collect_data.py
+++ collect_data.py
@@ -16,3 +16,8 @@ IMG_SIZE = 224
 CAPTURE_INTERVAL = 0.2
 DATASET_PATH = "dataset/raw"
 PADDING = 30 #Padding for bouding box aroung the hand in the image
+
+for gesture_name in GESTURES.values():
+    folder_path = os.path.join(DATASET_PATH, gesture_name)
+    os.makedirs(folder_path, exist_ok=True)
+    print(f"Created Folder: {folder_path}")
